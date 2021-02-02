ef47a331fe3fc457cb59f601424bbde481dff1bf
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2021-02-02 17:26:15 -0600

    Api changed 3

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://amazon-clone-1hw11lvg9.vercel.app/api/",
+  baseURL: "http://localhost:3000/api/",
 });
 
 export default instance;
