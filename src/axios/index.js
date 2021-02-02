172a9bd96749437ef98a20a7e8dc6a5f9c6955a9
José Juan Téllez Guzmán <jjtellezg@gmail.com>
2021-02-02 17:37:32 -0600

    Enabling Cors api

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://amazon-clone-1hw11lvg9.vercel.app/api",
+  baseURL: "http://localhost:3000/api",
 });
 
 export default instance;
-//baseURL: "http://localhost:3000/api/",
+
+//  baseURL: "https://amazon-clone-1hw11lvg9.vercel.app/api",
